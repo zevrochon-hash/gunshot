8be171aaa84fe822030a574a2f55509da27469d8
ZeOne <zev.rochon@gmail.com>
2026-09-21 13:26:34 -0500

    Fix build-go.sh: lower CGO min iOS version to 12.0 for iPhone 6

--- scripts/build-go.sh
+++ scripts/build-go.sh
@@ -6,6 +6,6 @@ mkdir -p .build
 sdk=$(xcrun --sdk iphoneos --show-sdk-path)
 cc=$(xcrun --sdk iphoneos --find clang)
 CGO_ENABLED=1 GOOS=ios GOARCH=arm64 CC="$cc" \
- CGO_CFLAGS="-isysroot $sdk -arch arm64 -miphoneos-version-min=15.0" \
- CGO_LDFLAGS="-isysroot $sdk -arch arm64 -miphoneos-version-min=15.0" \
+ CGO_CFLAGS="-isysroot $sdk -arch arm64 -miphoneos-version-min=12.0" \
+ CGO_LDFLAGS="-isysroot $sdk -arch arm64 -miphoneos-version-min=12.0" \
  go build -tags cli -buildmode=c-archive -trimpath -o .build/libgotohp.a ./cmd/bridge
